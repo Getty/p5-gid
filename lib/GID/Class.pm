7aa92db6cae13a2a39e64aed6dfe72ddd2e60052
Torsten Raudssus <torsten@raudss.us>
2012-12-18 18:20:54 +0100

    Yeah, let's use namespace::clean then!

--- lib/GID/Class.pm
+++ lib/GID/Class.pm
@@ -17,6 +17,7 @@ package GID::Class;
 use strictures 1;
 use Import::Into;
 use Scalar::Util qw( blessed );
+use namespace::clean ();
 
 use GID ();
 use MooX ();
@@ -38,32 +39,9 @@ sub import {
 		Types::MooseLike
 	));
 
-	$target->can('extends')->('GID::Object');
+	namespace::clean->import::into($target);
 
-	$target->class_stash->around_method('has',sub {
-		my $has = shift;
-		my $attribute_arg = shift;
-		my @attribute_args = @_;
-		my @attributes = ref $attribute_arg eq 'ARRAY' ? @{$attribute_arg} : ($attribute_arg);
-		for my $attribute (@attributes) {
-			if (grep { $attribute eq $_ } @gid_methods) {
-				my $gid_method = $target->class_stash->get_method($attribute);
-				$target->class_stash->remove_method($attribute);
-				$has->($attribute,@attribute_args);
-				$target->class_stash->around_method($attribute,sub {
-					my $attribute_method = shift;
-					my @args = @_;
-					if (blessed $args[0]) {
-						return $attribute_method->(@args);
-					} else {
-						return $gid_method->(@args);
-					}
-				});
-			} else {
-				$has->($attribute,@attribute_args);
-			}
-		}
-	});
+	$target->can('extends')->('GID::Object');
 
 }
 
